c808a04aa09a930970563da1e98acb84c23ba683
Sain-Biswas <sain.biswas614@gmail.com>
2025-12-22 23:31:45 +0530

    feat(server): add elysia server for backend request handling.

---
+++ apps/server/src/development.ts
@@ -0,0 +1,7 @@
+import rootApplication from "~/index";
+
+rootApplication.listen(3000);
+
+console.log(
+  `🦊 Elysia is running at ${rootApplication.server?.hostname}:${rootApplication.server?.port}`
+);
---
+++ apps/server/src/index.ts
@@ -0,0 +1,7 @@
+import { Elysia } from "elysia";
+
+const app = 
+  new Elysia()
+  .get("/", () => "Hello Elysia");
+
+export default app;
