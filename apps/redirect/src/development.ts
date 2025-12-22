a087ae1f5b8d5b00d2d095da3347202e411e3dbf
Sain-Biswas <sain.biswas614@gmail.com>
2025-12-22 23:39:39 +0530

    feat(redirect): add elysia redirect server to redirect users from short url to actual url

---
+++ apps/redirect/src/development.ts
@@ -0,0 +1,7 @@
+import rootApplication from "~/index";
+
+rootApplication.listen(8080);
+
+console.log(
+  `🦊 Elysia is running at ${rootApplication.server?.hostname}:${rootApplication.server?.port}`
+);
