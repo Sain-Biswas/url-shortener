6013d06f9a58da588db6430b037ab6e6c31bf313
Sain-Biswas <sain.biswas614@gmail.com>
2025-12-28 23:43:27 +0530

    feat(drizzle): update exports in package.json

--- packages/drizzle-orm/tsdown.config.ts
+++ packages/drizzle-orm/tsdown.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
-  exports: true,
+  exports: false,
+  clean: true,
+  dts: true,
+  format: "esm",
+  platform: "node",
   entry: {
     main: "src/index.ts",
     schema: "src/schema.ts"
-  },
-  dts: {
-    enabled: true
   }
 });
